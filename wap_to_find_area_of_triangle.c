107299d9fbffc2882040e0265fdfc3560a00a421
Divy Dangar <ahirdivu001@gmail.com>
2022-07-22 14:14:37 +0530

    Create wap_to_find_area_of_triangle.c

---
+++ wap_to_find_area_of_triangle.c
@@ -0,0 +1,14 @@
+//wap_to_find_area_of_triangle 
+#include<stdio.h>
+int main()
+{
+
+    float x,n1,n2;
+    printf("enter value of base = ");
+    scanf("%f",&n1);
+    printf("enter value of height = ");
+    scanf("%f",&n2);
+    x=(n1*n2)/2;
+    printf("Area of triangle is = %f",x);
+
+}
