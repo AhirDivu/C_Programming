c6772ca6495f456669aaa6905bea3491a8092852
Divy Dangar <ahirdivu001@gmail.com>
2022-07-22 14:14:18 +0530

    Create display_this_information_using_printf.c

---
+++ display_this_information_using_printf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{      
+     printf("Name : Divy Dangar");
+     printf("\nAge:21");
+     printf("\nBirthdate: 25th November 2001 \n");
+     printf("Adress: C.G.road, Ahmedabad");
+}
