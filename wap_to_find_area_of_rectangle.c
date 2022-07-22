dfdaa342394de04643d6a5c091f90dbcd067d32d
Divy Dangar <ahirdivu001@gmail.com>
2022-07-22 14:14:31 +0530

    Create wap_to_find_area_of_rectangle.c

---
+++ wap_to_find_area_of_rectangle.c
@@ -0,0 +1,13 @@
+//wap_to_find_area_of_rectangle
+
+#include<stdio.h>
+int main()
+{
+    float n1,n2,x;
+    printf("Enter of value height = ");
+    scanf("%f",&n1);
+    printf("Enter of value width = ");
+    scanf("%f",&n2);
+    x=n1*n2;
+    printf("Area Of Rectangle Is = %f",x);
+}
