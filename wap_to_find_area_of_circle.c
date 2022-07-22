fc28f8b0e9cb08a95f74c705ac5a3265cdd200ff
Divy Dangar <ahirdivu001@gmail.com>
2022-07-22 14:14:28 +0530

    Create wap_to_find_area_of_circle.c

---
+++ wap_to_find_area_of_circle.c
@@ -0,0 +1,17 @@
+//wap_to_find_area_of_circle, rectangle and triangle 
+
+#include<stdio.h>
+int main()
+{
+   
+    float r,a;
+    printf("Enter of value r:");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    
+    printf("Area of circle=%f",a);
+
+}
+
+
+
